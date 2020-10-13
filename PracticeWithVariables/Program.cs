26092b72832c455185a622189a46d0fb2e26e8ea
Khemarak Loeung <khemarakloeung01@gmail.com>
2020-10-13 15:54:28 -0500

    Initial Commit

---
+++ PracticeWithVariables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PracticeWithVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            bool conditional = true;
+            char letter = 'N';
+            string firstName = "Nathan";
+
+            
+
+
+        }
+    }
+}
